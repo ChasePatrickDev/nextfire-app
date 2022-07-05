cb2f260a3bd52239795809b788790fd0ab2bfb05
chasepatrick <91498359+chasepatrick@users.noreply.github.com>
2022-07-05 17:06:49 -0400

    Added ImageUploader and HeartButton

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,7 +1,7 @@
 import styles from "../../styles/Admin.module.css";
 import AuthCheck from "../../components/AuthCheck";
 import { firestore, auth, serverTimestamp } from "../../lib/firebase";
-import ImageUploader from "../../components/ImageUploader";
+import ImageUploader from "../../components/ImageUploader.js";
 
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -9,7 +9,7 @@ import { useRouter } from "next/router";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { useForm } from "react-hook-form";
 import ReactMarkdown from "react-markdown";
-import Link from "next/link";
+import Link from "next/Link";
 import Toast from "react-hot-toast";
 
 export default function AdminPostsEdit(props) {
@@ -40,7 +40,7 @@ function PostManager() {
 
             <PostForm
               postRef={postRef}
-              defaultValues={posts}
+              defaultValues={post}
               preview={preview}
             />
             <aside>
@@ -60,10 +60,18 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }) {
-  const { register, handleSubmit, reset, watch } = useForm({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+    formState,
+  } = useForm({
     defaultValues,
     mode: "onChange",
   });
+  const { isValid, isDirty } = formState;
 
   const updatePost = async ({ content, published }) => {
     await postRef.update({
@@ -72,7 +80,7 @@ function PostForm({ defaultValues, postRef, preview }) {
       updatedAt: serverTimestamp(),
     });
     reset({ content, published });
-    toast.success("Post updated Successfully");
+    Toast.success("Post updated Successfully");
   };
 
   return (
@@ -83,17 +91,32 @@ function PostForm({ defaultValues, postRef, preview }) {
         </div>
       )}
       <div className={preview ? styles.hidden : styles.controls}>
-        <textarea name="content" ref={register}></textarea>
+        <ImageUploader />
+
+        <textarea
+          {...register("content", {
+            maxLength: { value: 20000, message: "content is too long" },
+            minLength: { value: 10, message: "content is too short" },
+            required: { value: true, message: "content is required" },
+          })}
+        ></textarea>
+        {errors.content && (
+          <p className="text-danger">{errors.content.message}</p>
+        )}
       </div>
       <fieldset>
         <input
           className={styles.checkbox}
-          name="published"
           type="checkbox"
-          ref={register}
+          {...register("published")}
         />
+        <lable>Published</lable>
       </fieldset>
-      <button type="submit" className="btn-green">
+      <button
+        type="submit"
+        className="btn-green"
+        disabled={!isDirty || !isValid}
+      >
         Save Changes
       </button>
     </form>
