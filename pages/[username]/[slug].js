f5b52784aa21802eac9dce25bdaaed021dae8e88
chasepatrick <91498359+chasepatrick@users.noreply.github.com>
2022-07-05 10:08:19 -0400

    Implemented the Server Side Rendering

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,62 @@
-export default function PostPage({}) {
-  return <main></main>;
+import PostContent from "../../components/PostContent";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getStaticProps({ params }) {
+  const { getUserWithUsername, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // could imporove by  using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup("post").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    // { params: { username, slug}}
+    //],
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+  return (
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+      <aside className="card">
+        <p>
+          <string>{post.heartCount || 0} 🤍</string>
+        </p>
+      </aside>
+    </main>
+  );
 }
